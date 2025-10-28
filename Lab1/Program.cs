1583b8a20c8632c7cc8695e33e29393b2650111b
emadsu <109160381+emadsu@users.noreply.github.com>
2025-10-28 03:09:14 +0000

    Completed Lab1.

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Step 1 
+        Console.WriteLine("Emad Sumrain. IT-1050");
+
+        /*
+        Step 2 
+        Emad Sumrain
+        IT-1050
+        */
+
+        int favNumber = 29;
+        string favLanguage = "Python";
+        double programsWrote = 2.6;
+        bool programmingExp = true;
+
+        Console.WriteLine("My Favorite Number?: " + favNumber);
+        Console.WriteLine("Favorite Programming Language?: " + favLanguage);
+        Console.WriteLine("Programs Written Before? " + programsWrote);
+        Console.WriteLine("Experience?: " + programmingExp);
+
+        const string SCHOOL = "Cuyahoga Community College";
+
+        Console.WriteLine("I go to " + SCHOOL);
     }
+    
+
 }
--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8176526fca6509dae3ac8414dea98f0955504297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f73dd10783d5ad21f83be3c8cc73d57ed403bf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
