6c8ab359b5fec3fd0366105f579ac803b3d00e73
emadsu <109160381+emadsu@users.noreply.github.com>
2025-12-02 03:58:19 +0000

    savingdsgfgshfjfhh

--- Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edf58f4ce795f10ad38b27cbea5ad730f8da1cd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6861ff5488441611f6fddd4ad258de559723eca5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
