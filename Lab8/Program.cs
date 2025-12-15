983bf4af019ed1f084f54d08147f5dc88c54637c
emadsu <109160381+emadsu@users.noreply.github.com>
2025-12-15 00:56:16 +0000

    finished lab 8

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -1,9 +1,66 @@
-﻿namespace Lab8;
+﻿using System;
+
+namespace Lab8;
+
+// Emad S
+// IT-1050
+// CRN: 82045
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Furniture myFurniture = new Furniture(); // Create a Furniture object
+            myFurniture.FurnitureDetail();
+
+            // Step 5 - Create an object of type Dresser. Call its FurnitureDetail() method.
+            Dresser myDresser = new Dresser();
+            myDresser.FurnitureDetail();
+
+            // Step 6 - Create an object of type Desk. Call its FurnitureDetail() method.
+            Desk myDesk = new Desk();
+            myDesk.FurnitureDetail();
+
+            // Step 7 - Create an object of type Bed. Call its FurnitureDetail() method.
+            Bed myBed = new Bed();
+            myBed.FurnitureDetail();
+    } // end of main
+} // end of class
+
+  //Given class Furniture with method FurnitureDetail( ) that outputs "The furniture is made of wood."
+  //Please leave the following Furniture class code in your lab
+  class Furniture  // Base class (parent) 
+  {
+    public virtual void FurnitureDetail()  
+    {
+      Console.WriteLine("The furniture is made of wood.");
     }
-}
+  }   //end of class Furniture
+
+  //Step 2 - Create a derived (child) class called Dresser. Add a method called FurnitureDetail() that outputs: "The dresser is made from cherry    wood."
+ class Dresser : Furniture
+    {
+        public override void FurnitureDetail()
+        {
+            Console.WriteLine("The dresser is made from cherry wood.");
+        }
+    }
+
+  //Step 3 - Create a derived (child) class called Desk. Add a method called FurnitureDetail() that outputs: "The desk is made from oak wood."
+ class Desk : Furniture
+    {
+        public override void FurnitureDetail()
+        {
+            Console.WriteLine("The desk is made from oak wood.");
+        }
+    }
+
+ //Step 4 - Create a derived (child) class called Bed. Add a method called FurnitureDetail() that outputs: "The bed is made from walnut wood."
+ class Bed : Furniture
+    {
+        public override void FurnitureDetail()
+        {
+            Console.WriteLine("The bed is made from walnut wood.");
+        }
+    }
+ //end of namespace Lab8
--- Lab8/obj/Debug/net8.0/Lab8.AssemblyInfo.cs
+++ Lab8/obj/Debug/net8.0/Lab8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e1eea70f7f965dc58fe11fbe1a1933c73f9dfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda1f46db8f249b321836f74a163c6580a644f30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
